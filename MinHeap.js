4acf97b1ed5796d9166ca1cc95104626e18ccac0
Michael Pompili <michael@churroboros.com>
2018-06-11 19:51:58 -0700

    finished minHeap

--- MinHeap.js
+++ MinHeap.js
@@ -29,29 +29,33 @@ export default class MinHeap {
     // returns the index positions 
 
     leftChild(index) {
-        return (index * 2) + 1;
+        let idx = (index * 2) + 1;
+        return idx >= 0 ? idx : null;
     }
 
     rightChild(index) {
-        return (index * 2) + 2;
+        let idx = (index * 2) + 2;
+        return idx >= 0 ? idx : null;
     }
 
     parentOf(index) {
-        return Math.floor((index - 1) / 2); 
+        let idx = Math.floor((index - 1) / 2); 
+        return idx >= 0 ? idx : null;
     }
 
     add(value) {
         this.array.push(value); 
         this.heapifyUp(); 
+        return this; //return the MinHeap
     }
 
     minPeek() {
         return this.array[0]; 
     }
 
-    heapifyUp() {
+    heapifyUp(customIdx) {
         
-        let idx = this.array.length - 1; 
+        let idx = customIdx || this.array.length - 1; 
         console.log('before heapifyUp: ', this.array); 
         while (this.array[this.parentOf(idx)] > this.array[idx] ){
             this.array.swap(this.parentOf(idx), idx); 
@@ -63,19 +67,18 @@ export default class MinHeap {
 
     extract() {
         //always extracts root
+        let extracted = this.array[0];
         console.log('this is before extract: ', this.array); 
-        let min = this.array.shift(); 
-        // puts last node at the front; 
-        this.array.unshift(this.array.pop()); 
+        this.array[0] = this.array.pop(); 
         this.heapifyDown(); 
         console.warn('after extract: ', this.array); 
-        return min; 
+        return extracted; 
     }
 
-    heapifyDown() {
+    heapifyDown(customIdx) {
         let smallestIdx; 
         console.log('this in heapifyDown: ', this); 
-        let nodeIdx = 0; 
+        let nodeIdx = customIdx || 0; 
         while (this.array[nodeIdx] > this.array[this.leftChild(nodeIdx)]) {
             smallestIdx = this.leftChild(nodeIdx); 
             if (this.array[smallestIdx] > this.array[this.rightChild(0)]){
@@ -86,6 +89,44 @@ export default class MinHeap {
             // after swap top would be what was at 2, and 2 would be what is at 
             nodeIdx = smallestIdx; 
         }
-        return this; 
+        //no need to return anything just changing the array 
     }
+
+    find(item) {
+        let foundItems = []; 
+
+        for (let idx = 0; idx < this.array.length; idx++) {
+            if (this.array[idx] === item) {
+                foundItems.push(idx); 
+            }
+        }
+        return foundItems; 
+    }
+
+    remove(item) {
+        const itemsToRemove = this.find(item); 
+        const numToRemove = itemsToRemove.length; 
+        for (let i = 0; i < numToRemove; i++) {
+            let indexToRemove = itemsToRemove.pop(); 
+        
+            // if index to remove is at the end just pop it
+            if (indexToRemove === (this.array.length - 1)) {
+                this.array.pop(); 
+            } else {
+                //otherwise
+                //if the indexToRemove has a left child and its parents are nonexistent or smaller than itself, heapifyDown; 
+                let parentIdx = this.parentOf(indexToRemove); 
+                let leftChild = this.leftChild(indexToRemove); 
+                if (leftChild && (!parentIdx || this.array[parentIdx] < this.array[indexToRemove])){
+                    this.heapifyDown(indexToRemove); 
+                }else{
+                    //otherwise heapifyUp; 
+                    this.heapifyUp(indexToRemove); 
+                }
+
+            }
+        }
+    return this; 
+    }
+
 }

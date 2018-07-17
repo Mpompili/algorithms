872bf620c0531574feabd5da72ed808448631c90
Michael Pompili <michael@churroboros.com>
2018-07-16 22:45:23 -0700

    3.4

--- ctci_stack_queues.js/problems.js
+++ ctci_stack_queues.js/problems.js
@@ -79,3 +79,85 @@ class SetOfStacks {
     }
 
 }
+
+// 3.4 using an additional array to hold 
+class MyQueue {
+    //we can use other data structures. 
+    constructor() {
+        this.deq = new QStack;
+        this.enq = new QStack;
+    }
+
+    enqueue(val) {
+        const { deq, enq } = this;
+
+        deq.isEmpty()
+            ? deq.push(val)
+            : enq.push(val);
+    }
+
+    dequeue() {
+        let { deq, enq } = this;
+        if (deq.isEmpty() || (!deq.isEmpty() && enq.isEmpty())) console.log("there aint no numbers in here");
+
+        let response = deq.pop();
+        let temp = enq.unload();
+        let newDeq = temp.pop();
+        deq.push(newDeq);
+        temp.forEach(num => enq.push(num));
+        return response;
+    }
+
+}
+
+class QStack {
+    constructor() {
+        this.stack = [];
+    }
+
+    push(val) {
+        this.stack.push(val);
+    }
+
+    pop(flag = false) {
+        return this.stack.pop();
+    }
+
+    unload() {
+        let times = this.stack.length;
+        let load = [];
+        for (let i = 0; i < times; i++) {
+            let top = this.stack.pop();
+            load.unshift(top);
+        }
+        return load;
+    }
+
+    isEmpty() {
+        return this.stack.length === 0;
+    }
+}
+
+// let myQ = new MyQueue;
+// myQ.dequeue();
+// myQ.enqueue('first');
+// myQ.enqueue(1);
+// myQ.enqueue(2);
+// myQ.enqueue(3);
+// myQ.enqueue(4);
+// myQ.enqueue('second');
+
+// console.log(myQ);
+// myQ.dequeue();
+// console.log(myQ);
+// myQ.dequeue();
+// console.log(myQ);
+// myQ.dequeue();
+// console.log(myQ);
+// myQ.dequeue();
+// console.log(myQ);
+// myQ.dequeue();
+// console.log(myQ);
+// myQ.dequeue();
+// console.log(myQ);
+// myQ.dequeue(); 

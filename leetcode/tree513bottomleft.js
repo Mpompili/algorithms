75ef7adbe28adb3e1caa074fda10d96e7a9a86ae
Michael Pompili <michael@churroboros.com>
2018-08-01 16:27:32 -0700

    solved tree 513 bottom left

---
+++ leetcode/tree513bottomleft.js
@@ -0,0 +1,17 @@
+//Given a binary tree, find the leftmost value in the last row of the tree.
+
+// by doing a BFS and storing the left child last the last node examined in the queue will always be the leftmost node. 
+
+
+var findBottomLeftValue = function (root) {
+    let queue = [root, 'end'];
+
+    let lastnode;
+    while (queue.length > 0) {
+        lastnode = queue.shift();
+        if (lastnode.right) queue.push(lastnode.right);
+        if (lastnode.left) queue.push(lastnode.left);
+    }
+    return lastnode.val
+
+};

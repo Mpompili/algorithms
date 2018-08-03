90d2b2d1716c6a5a9973418e066afe5a708605b3
Michael Pompili <michael@churroboros.com>
2018-08-03 08:28:27 -0700

    practice with tries

--- trie_practice.js
+++ trie_practice.js
@@ -13,15 +13,21 @@ Trie.prototype.add = function(key) {
     key = key.slice(1); 
 
     // if the next node has curren character as child, just move on. 
-    while (curNode.children[curChar] !== 'undefined' && key.length > 0) {
+    while (curNode.children[curChar] !== 'undefined' && curChar.length > 0) {
         curNode = curNode.children[curChar]; 
         curChar = key.slice(0,1); 
         key = key.slice(1); 
     }
 
-    while (key.length > 0) {
-        let newNode = new Node(curChar); 
+    while (curChar.length > 0) {
+        let newNode = { 
+            key: curChar,
+            value: key.length === 0 ? null : undefined,
+            children: {}
+        };
         curNode.children[curChar] = newNode; 
-
+        curNode = newNode; 
+        curChar = key.slice(0,1);
+        key = key.slice(1); 
     }
 };

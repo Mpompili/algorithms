2ada5d89396fdc105ea143935abbae098d10100a
Michael Pompili <michael@churroboros.com>
2018-08-05 12:21:14 -0700

    created own simplified version of trie

--- trie_practice.js
+++ trie_practice.js
@@ -1,3 +1,4 @@
+//V1 from online 
 class Trie {
     constructor(){
         this.head = {
@@ -31,3 +32,114 @@ Trie.prototype.add = function(key) {
         key = key.slice(1); 
     }
 };
+
+
+//V2 my version 
+class Trie2 {
+    constructor() {
+        this.trie = {};
+    }
+}
+
+Trie2.prototype.add = function(word) {
+    let current = this.trie;
+    let char = word.slice(0, 1);
+    word = word.slice(1);
+    let count = 0;
+    while (char.length > 0) {
+        if (current[char] === undefined) current[char] = {};
+        current = current[char];
+        char = word.slice(0, 1);
+        word = word.slice(1);
+        count += 1;
+    }
+    current['end'] = count;
+    return current;
+};
+
+Trie2.prototype.search = function(word) {
+    let current = this.trie;
+    let char = word.slice(0, 1);
+    word = word.slice(1);
+    while (char.length > 0) {
+        if (current[char] === undefined) return false;
+        current = current[char];
+        char = word.slice(0, 1);
+        word = word.slice(1);
+    }
+    return current.end || 'contained';
+};
+
+Trie2.prototype.remove = function(word) {
+    if (!this.search(word)) return 'no';
+    let current = this.trie;
+    let char = word.slice(0, 1);
+    word = word.slice(1);
+    while (char.length > 0) {
+        if (Object.keys(current[char]).length > 1) {
+            current = current[char];
+            char = word.slice(0, 1);
+            word = word.slice(1);
+        } else {
+            delete current[char];
+            return this.trie;
+        }
+    }
+};
+
+
+
+let test = new Trie();
+test.add('hello');
+test.add('help');
+test.add('homo');
+test.remove('homo');
+// test.trie
+// test.remove('homo');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function createTrie(word, trie = {}) {
+//   let char = word.slice(0,1);
+//   word = word.slice(1); 
+//   let current = trie; 
+//   while (char.length > 0) {
+//     if (current[char] === undefined) {
+//       current[char] = {};
+//     } 
+//     current = current[char]; 
+//     char = word.slice(0,1); 
+//     word = word.slice(1); 
+//   }
+//   current['end'] = true;
+//   // console.log('current: ', current, ' char: ', char); 
+//   return trie; 
+// }
+
+// let ret = createTrie('hello');
+// ret = createTrie('help', ret);
+
+// function s
+
+
